c93933e50bb3541e66e4813565d4334a7b45c920
Ashley Brener <ashley@starlogik.com>
2015-04-26 23:41:02 +0200

    refactored to use grunt-modularize

--- Gruntfile.js
+++ Gruntfile.js
@@ -57,6 +57,8 @@ module.exports = function (grunt) {
   // Actually load this plugin's task(s).
   grunt.loadTasks('tasks');
 
+  grunt.loadNpmTasks('grunt-modularize');
+
   // Whenever the "test" task is run, first clean the "tmp" dir, then run this
   // plugin's task(s), then test the result.
   grunt.registerTask('test', ['clean', 'riot:compile', 'nodeunit']);
--- tasks/riot.js
+++ tasks/riot.js
@@ -15,8 +15,6 @@ module.exports = function (grunt) {
 		concat = require('concat-stream');
 
 	grunt.registerMultiTask('riot', 'riot custom tag compiler plugin', function () {
-		var self = this;
-
 		var options = this.options({
 			compact : true ,
 			expr : true ,
@@ -51,50 +49,6 @@ module.exports = function (grunt) {
 			grunt.log.writeln('File ' + path + ' created.');
 		};
 
-		var generateModularHeader = function () {
-			var modules = options.modular instanceof Array ? options.modular : ['riot'];
-			var params = '';
-			var amdRequired = '';
-			var commonJSRequired = '';
-
-			modules.forEach(function (m, i) {
-				if (typeof(m) === 'string') {
-					var obj = {};
-					obj[m] = m;
-					m = obj;
-				}
-
-				for (var path in m) {
-					var param = m[path];
-
-					amdRequired += "'" + path + "'";
-					commonJSRequired += '\t\tvar ' + param + ' = require(\'' + path + '\');\n';
-					params += param;
-
-					if (i < modules.length - 1) {
-						amdRequired += ', ';
-						params += ', ';
-					}
-				}
-			});
-
-			var header = '';
-			header += '(function(tagger) {\n';
-			header += '	if (typeof define === \'function\' && define.amd) {\n';
-			header += '		define([' + amdRequired + '], function(' + params + ') {\n';
-			header += '			tagger(' + params + ');\n';
-			header += '		});\n';
-			header += '	} else if (typeof module !== \'undefined\' && typeof module.exports !== \'undefined\') {\n';
-			header += commonJSRequired;
-			header += '		tagger(' + params + ');\n';
-			header += '	} else {\n';
-			header += '		tagger(window.riot);\n';
-			header += '	}\n';
-			header += '})(function(' + params + ') {\n';
-
-			return header;
-		};
-
 		function getOptions(file, options) {
 			return options.fileConfig ? options.fileConfig(file,clone(options)) : options;
 		}
@@ -106,10 +60,6 @@ module.exports = function (grunt) {
 			};
 		});
 
-		var mHeader = generateModularHeader();
-
-		var mFooter = '});';
-
 		validFiles.forEach(function (files, x) {
 			if(options.concat){
 				var strings = concat(function(out) {
@@ -120,15 +70,7 @@ module.exports = function (grunt) {
 				});
 				files.src.map(function(file, y){
 					var fileSource = compileRiot( grunt.file.read(file) , getOptions(file,options) );
-
-					if (options.modular && x === 0 && y === 0)
-						strings.write(mHeader);
-
 					strings.write(fileSource + '\n');
-
-					if (options.modular && !validFiles[x + 1] && y === files.src.length - 1)
-						strings.write(mFooter);
-
 				});
 				strings.end();
 			}else{
@@ -140,8 +82,21 @@ module.exports = function (grunt) {
 				});
 			}
 
-		});
+			if (options.modular) {
+				var srcDest = {};
+				var absolutePath = path.join(__dirname, '../', files.dest);
+				srcDest[absolutePath] = absolutePath;
 
+				grunt.config.set('modularize.compile', {
+					options: {
+						deps: options.modular instanceof Array ? options.modular : ['riot']
+					},
+					files: srcDest
+				});
+
+				grunt.task.run(['modularize:compile']);
+			}
+		});
 	});
 
 };
